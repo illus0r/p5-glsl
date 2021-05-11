f59742a10a29230013f6e363c51acac72581a764
illus0r <zor667@gmail.com>
2021-05-12 01:47:00 +0300

    done p5 and twgl integration

--- index.js
+++ index.js
@@ -1,88 +1,71 @@
 'use strict';
 let twgl = require('twgl.js')
 
-
 // setting up canvas
-var charCanvas = document.createElement('canvas');
-charCanvas.width = 512
-charCanvas.height = 512
-var charContext = charCanvas.getContext("2d");
-charContext.fillStyle = "blue";
-charContext.fillRect(0, 0, charCanvas.width, charCanvas.height);
-charContext.fillStyle = "black";
-charContext.fillRect(100, 100, charCanvas.width - 200, charCanvas.height - 200);
+let canvasP5, ctx
+let fShader
+let canvasGl
+let gl
+let program
+let positionObject
+let positionBuffer
 
+let timeStart = +new Date();
 
-let sketch = function(p) {
-  let x = 100;
-  let y = 100;
-  p.setup = function() {
-    charCanvas = p.createCanvas(700, 410).elt;
-    charContext = charCanvas.getContext("2d");
-  };
-  p.draw = function() {
+let sketch = function (p) {
+  p.setup = function () {
+    canvasP5 = p.createCanvas(512, 512).elt;
+
+    // shader stuff
+    setupShader()
+  }
+
+  p.draw = function () {
     p.background(0);
     p.fill(255);
-    p.rect(x, y, 50, 50);
+    p.circle(256 + p.sin(p.frameCount*.1) * 100, 256 + p.cos(p.frameCount*.1) * 100, 50);
+
+    // shader stuff
+    drawShader(p.frameCount)
   };
 };
 let myp5 = new p5(sketch);
 
 
-const vShader = `#version 300 es
+function setupShader() {
+  ctx = canvasP5.getContext("2d");
+  const vShader = `#version 300 es
   precision mediump float;
   in vec2 position;
   void main() {
     gl_Position = vec4(position, 0.0, 1.0);
-  }`;
+  }`
+  fShader = require('./shader.frag')
+  canvasGl = document.getElementById('gl')
+  gl = canvasGl.getContext('webgl2', { premultipliedAlpha: false })
+  twgl.addExtensionsToContext(gl)
+  program = twgl.createProgramInfo(gl, [vShader, fShader])
+  positionObject = { position: { data: [1, 1, 1, -1, -1, -1, -1, 1], numComponents: 2 } }
+  positionBuffer = twgl.createBufferInfoFromArrays(gl, positionObject)
+}
 
-const fShader = require('./shader.frag');
-
-let tick = 0
-let timeLounch = +new Date()
-const canvas = document.getElementById('gl');
-const gl = canvas.getContext('webgl2', { premultipliedAlpha: false });
-twgl.addExtensionsToContext(gl);
-
-const program = twgl.createProgramInfo(gl, [vShader, fShader]);
-
-const positionObject = { position: { data: [1, 1, 1, -1, -1, -1, -1, 1], numComponents: 2 } };
-const positionBuffer = twgl.createBufferInfoFromArrays(gl, positionObject);
-
-let dt;
-let prevTime;
-
-function draw(time) {
-
+function drawShader(tick) {
   let texture = twgl.createTexture(gl, {
-    src: charContext.canvas,
+    src: ctx.canvas,
     format: gl.RGB,
     min: gl.LINEAR,
     wrap: gl.CLAMP_TO_EDGE,
   })
-  
   twgl.resizeCanvasToDisplaySize(gl.canvas);
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  dt = (prevTime) ? time - prevTime : 0;
-  prevTime = time;
-
-  gl.useProgram(program.program);
-  twgl.setBuffersAndAttributes(gl, program, positionBuffer);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+  gl.useProgram(program.program)
+  twgl.setBuffersAndAttributes(gl, program, positionBuffer)
   twgl.setUniforms(program, {
     u_texture: texture,
-    tick: tick,
-    u_time: (new Date() - timeLounch) / 1000,
+    u_tick: tick,
+    u_time: (new Date() - timeStart) / 1000,
     u_resolution: [gl.canvas.clientWidth, gl.canvas.clientHeight],
-  });
-
+  })
   twgl.bindFramebufferInfo(gl, null)
   twgl.drawBufferInfo(gl, positionBuffer, gl.TRIANGLE_FAN)
-  tick++
 }
-
-
-// Should be replaced with a p5 draw cycle?
-(function animate(now) {
-  draw(now / 1000);
-  requestAnimationFrame(animate)
-})(0);
--- shader.frag
+++ shader.frag
@@ -3,7 +3,7 @@ precision mediump float;
 uniform sampler2D u_texture;
 // uniform vec2 u_texture_resolution;
 
-uniform float tick;
+uniform float u_tick;
 uniform float u_time;
 uniform vec2 u_resolution;
 out vec4 outColor;
@@ -12,7 +12,7 @@ out vec4 outColor;
 
 void main() {
     vec2 uv = gl_FragCoord.xy / u_resolution;
-	outColor.rgb = texture(u_texture, uv).rgb;
+	outColor.rgb = texture(u_texture, vec2(uv.x, 1.-uv.y)).rgb;
 	outColor.rgb += .8 * rnd(length(gl_FragCoord.xy + vec2(123.456, 31.171) + u_time));
 	outColor.a = 1.;
 	outColor = clamp(outColor, 0., 1.);
